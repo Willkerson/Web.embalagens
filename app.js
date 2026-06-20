6436b9ff8c8fedfd5175c6fbe9f5bca3c7f16967
Willkerson Barcelar <52581026+Willkerson@users.noreply.github.com>
2026-06-20 11:32:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("APP OK");
 
 // ── CONFIG 2v ──
-const GH_TOKEN = "SEU_TOKEN_AQUI";
+const GH_TOKEN = "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";
 const REPO = "Willkerson/Automacao-ConnectPlug";
 const PATH = "fila/movimentacao.json";
 
